5f42938513bba56158c8341ae5c8eb8f88ad496b
AdahMilly <oamildred@gmail.com>
2021-02-04 12:59:15 +0300

    ..

---
+++ Project/example.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int var = 3;
+    var = 4; //Reassigning variables in c
+    printf("%d",var);
+    return 0;
+}
+int mn()
+{
+    int var= 5;
+    printf("%d", var);
+    return 0;
+}
